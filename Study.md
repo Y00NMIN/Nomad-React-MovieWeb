6e97a04d25ccd6577bdad8eaf2518f5f5de2becf
SungMin Yoon <yoonmin.tech@gmail.com>
2022-04-18 22:49:50 +0900

    Study-React-Effect Effect는 매번 새로운 ui를 업데이트하는 리액트에 동작을 제어해주는 역할을 해준다.

--- Study.md
+++ Study.md
@@ -8,6 +8,7 @@
 |   React    |    State    | <a href="https://github.com/Y00NMIN/Nomad-React-MovieWeb/commit/7dce3b32f6f71d02bed875ec2b358351fe93afc1">📄</a> |
 |   React    |   Prop-1    | <a href="https://github.com/Y00NMIN/Nomad-React-MovieWeb/commit/23968afb910ce08b501fcb0461d308a777a96563">📄</a> |
 |   React    |   Prop-2    | <a href="https://github.com/Y00NMIN/Nomad-React-MovieWeb/commit/e44545b0e172fa1aeee9117503c6cf62f6e0ffad">📄</a> |
+|   React    |   Effect    |                                                <a href="">📄</a>                                                 |
 
 #### 🏆🏆Create Convert (convert hours to minutes / kil)
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 
+import { useState, useEffect } from "react"; /* State 선언 */
+
+/* Use Effect function은 코드가 딱 한번만 실행될 수 있도록 보호해준다.
+  2개 component 사용 useEffect( function , [state.data] <-리액트가 지켜봐야할 대상) */
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((before) => before + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all time");
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("Counter :", counter);
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run keyword & counter change");
+  }, [keyword, counter]);
+  /* [] <= dependency(리액트가 지켜봐야할 대상
+    []만 있을 시 한번만 호출 | []안에 state.data값이 들어있을시 해당 data가 동작하면 출력 */
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        type="text"
+        placeholder="Search here"
+        value={keyword}
+        onChange={onChange}
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
-
+/* */
 export default App;
