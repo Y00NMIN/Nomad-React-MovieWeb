fd3dd6297de5c74dfa5b5872fa3a00663b061337
SungMin Yoon <yoonmin.tech@gmail.com>
2022-04-20 23:23:33 +0900

    chore: Introduce Movie Poster Page 평점별 영화 리스트 api를 통해 간단한 웹 페이지를 만들었다. 현 뼈대만 있다. 스타일 사용 X

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 
-import { useState, useEffect } from "react"; /* State 선언 */
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    /*수정하는 함수를 사용할대 2가지 옵션 (하단) 1.저장한 data값을 보낼때 사용 2. 함수를 보내는 방법 */
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    /* array 안에 arrry를 추가했을때 한 array로 동작하기 위해 array앞에 ...을 앞에 붙여야한다. */
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+  console.log(movies);
   return (
-    <div>
-      <h1>To Do List (List up : {toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do...."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+    <div className="App">
+      {loading ? (
+        <h1>Loading..</h1>
+      ) : (
+        <div className={styles.Movie_list}>
+          {movies.map((movie) => (
+            <div key={movie.id} className={styles.Movie_Poster}>
+              <img src={movie.medium_cover_image} />
+              <div className={styles.Movie_Poster_desc}>
+                <h2>{movie.title}</h2>
+                <p>{movie.summary}</p>
+                <ul>
+                  {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-    /* map 함수에 item 값에 index를 부여하지 않으면 console에서 오류가 발생하기 때문에 li 안에 key값을 부여해야한다, */
   );
 }
-
 export default App;
