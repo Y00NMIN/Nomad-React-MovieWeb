6d042df57fe6b7be0810af9b741613425caf6549
SungMin Yoon <yoonmin.tech@gmail.com>
2022-04-21 23:00:48 +0900

    Refactor: Movie Web page route 기능 추가 및 기존 app.js에 있던  jsx 코드 route home.js 이동

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []);
-  console.log(movies);
   return (
-    <div className="App">
-      {loading ? (
-        <h1>Loading..</h1>
-      ) : (
-        <div className={styles.Movie_list}>
-          {movies.map((movie) => (
-            <div key={movie.id} className={styles.Movie_Poster}>
-              <img src={movie.medium_cover_image} />
-              <div className={styles.Movie_Poster_desc}>
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
 export default App;
